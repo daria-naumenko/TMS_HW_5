8e73b4db760c6f317527b05084928fd3481f226f
Darya Naumenka <naumenka.daria@gmail.com>
2023-04-10 16:43:06 +0300

    lesson5

---
+++ src/main/java/firstTask/Student.java
@@ -0,0 +1,51 @@
+package firstTask;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private String sex;
+    private int course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", course=" + course +
+                '}';
+    }
+}
